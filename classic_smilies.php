a3f7fa60b033f1a63eecbbf936df12683f8c48fd
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-25 20:27:32 +0000

    First upload of the classic smilies plugin
    git-svn-id: https://plugins.svn.wordpress.org/classic-smilies/trunk@881947 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classic_smilies.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Classic Smilies
+Plugin URI: http://ottopress.com/wordpress-plugins/classic-smilies/
+Description: Puts back the original smilies.
+Version: 1.0
+Author: Otto
+Author URI: Author URI: http://ottodestruct.com
+License: GPLv2
+License URI: 
+Text Domain: classic-smilies
+*/
+
+// fix the path to the smilies to point to the plugin
+function classic_smilies_src( $old, $img ) {
+	return plugins_url( "img/{$img}", __FILE__ );
+}
+	
+// force the use of the classic smilies
+function classic_smilies_init() {
+	global $wpsmiliestrans;
+
+	$wpsmiliestrans = array(
+	':mrgreen:' => 'icon_mrgreen.gif',
+	':neutral:' => 'icon_neutral.gif',
+	':twisted:' => 'icon_twisted.gif',
+	  ':arrow:' => 'icon_arrow.gif',
+	  ':shock:' => 'icon_eek.gif',
+	  ':smile:' => 'icon_smile.gif',
+	    ':???:' => 'icon_confused.gif',
+	   ':cool:' => 'icon_cool.gif',
+	   ':evil:' => 'icon_evil.gif',
+	   ':grin:' => 'icon_biggrin.gif',
+	   ':idea:' => 'icon_idea.gif',
+	   ':oops:' => 'icon_redface.gif',
+	   ':razz:' => 'icon_razz.gif',
+	   ':roll:' => 'icon_rolleyes.gif',
+	   ':wink:' => 'icon_wink.gif',
+	    ':cry:' => 'icon_cry.gif',
+	    ':eek:' => 'icon_surprised.gif',
+	    ':lol:' => 'icon_lol.gif',
+	    ':mad:' => 'icon_mad.gif',
+	    ':sad:' => 'icon_sad.gif',
+	      '8-)' => 'icon_cool.gif',
+	      '8-O' => 'icon_eek.gif',
+	      ':-(' => 'icon_sad.gif',
+	      ':-)' => 'icon_smile.gif',
+	      ':-?' => 'icon_confused.gif',
+	      ':-D' => 'icon_biggrin.gif',
+	      ':-P' => 'icon_razz.gif',
+	      ':-o' => 'icon_surprised.gif',
+	      ':-x' => 'icon_mad.gif',
+	      ':-|' => 'icon_neutral.gif',
+	      ';-)' => 'icon_wink.gif',
+	// This one transformation breaks regular text with frequency.
+	//     '8)' => 'icon_cool.gif',
+	       '8O' => 'icon_eek.gif',
+	       ':(' => 'icon_sad.gif',
+	       ':)' => 'icon_smile.gif',
+	       ':?' => 'icon_confused.gif',
+	       ':D' => 'icon_biggrin.gif',
+	       ':P' => 'icon_razz.gif',
+	       ':o' => 'icon_surprised.gif',
+	       ':x' => 'icon_mad.gif',
+	       ':|' => 'icon_neutral.gif',
+	       ';)' => 'icon_wink.gif',
+	      ':!:' => 'icon_exclaim.gif',
+	      ':?:' => 'icon_question.gif',
+	);
+
+
+	add_filter( 'smilies_src', 'classic_smilies_src', 10, 2 );
+}
+
+add_action( 'init', 'classic_smilies_init', 1 );
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Classic Smilies ===
+Contributors: Otto42
+Plugin Name: Classic Smilies
+Tags: smilies, classic, better looking
+Requires at least: 3.9
+Tested up to: 3.9
+Stable tag: trunk
+
+Replace the smilies with the original versions from previous versions of WordPress.
+
+== Description ==
+
+Replace the smilies with the original versions from previous versions of WordPress.
+
+== Changelog ==
+
+= 1.0 = 
+* First version
